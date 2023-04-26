9d834793b90ae340008391d60aab89cce2352a2a
Janne Marie <janne_m_tvetene@hotmail.com>
2023-04-26 11:31:53 +0200

    commit

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ app.use(express.json());
 
 const config = {
   authRequired: false,
-  auth0Logout: true
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL:  process.env.ISSUER_BASE_URL
 };
 
 const port = process.env.PORT || 3000;
